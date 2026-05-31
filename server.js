cf93007ae0370057bd3353e233135645f8ec65a7
Hariharan <harishrajkumar2003@gmail.com>
2026-05-31 15:42:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
+app.use(express.json());
+
+let messages = [];
+
+app.get("/messages", (req, res) => {
+  const { user1, user2 } = req.query;
+
+  const filteredMessages = messages.filter(
+    (msg) =>
+      (msg.sender === user1 && msg.receiver === user2) ||
+      (msg.sender === user2 && msg.receiver === user1)
+  );
+
+  res.json(filteredMessages);
+});
+
+app.post("/messages", (req, res) => {
+  messages.push(req.body);
+
+  res.json({
+    success: true
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
